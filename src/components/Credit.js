1ea55b90731032f83efcab597c9ef68d8e8a7fcb
AnshulMishra19 <mishraanshul738@gmail.com>
2024-07-07 13:01:33 +0530

    Initial commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Made by:
+      <a href="https://www.linkedin.com/in/anshulmishra74/"
+        > Anshul Mishra</a
+      >, <a href="">Github Link</a> 
+    </p>
+  </div>
+  );
+}
